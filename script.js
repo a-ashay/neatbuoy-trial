36d30d8fc7aa0de37525452805e41fcfeea69191
a-ashay <184088492+a-ashay@users.noreply.github.com>
2024-10-28 04:05:58 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// Register ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+// Timeline for scroll-dependent animations
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".container",
+    start: "top top", 
+    end: "bottom+=200%", 
+    scrub: 1, 
+    pin: true, 
+  }
+});
+
+// Circle animation - moves out of viewport
+tl.to(".left-circle", {
+  x: "-80vw",
+  ease: "none", 
+});
+
+tl.to(".right-circle", {
+  x: "80vw",
+  ease: "none", 
+}, "<");
+
+// Triangle animation - moves up and expands
+tl.to(".triangle", {
+  y: "-80vh", 
+  ease: "power1.out", 
+}, "<");
+
+tl.to(".triangle", {
+  scale: 20, 
+  ease: "power2.inOut", 
+});
+
+// Target all <p> elements inside the .other section
+gsap.from(".other p", {
+  opacity: 0,
+  y: 20, // Optional: adds a slight upward animation
+  duration: 1,
+  stagger: 2, 
+  scrollTrigger: {
+    trigger: ".other",
+    start: "top center", 
+  },
+});
+
